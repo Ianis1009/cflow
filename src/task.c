d4653d59416803bebce48c0de2b44cf5c101238d
Iosif Ianis-Cosmin <iosifianis1@gmail.com>
2026-09-13 23:06:56 +0300

    update task.c

--- src/task.c
+++ src/task.c
@@ -6,12 +6,47 @@
 
 static char* string_duplicate (const char* source) {
 
-    //TODO
+    if (source == NULL) {
+        return NULL;
+    }
+
+    size_t length = strlen(source);
+    char *copy = (char *)malloc(sizeof(char) * (length + 1));
+
+    if (copy == NULL) {
+        return NULL;
+    }
+
+    memcpy(copy, source, length + 1);
+    return copy;
 }
+
+
 
 Task* task_create(int id, const char *title, TaskPriority priority) {
 
-    //TODO
+    if (title == NULL) {
+        return NULL;
+    }
+
+    Task *task = malloc (sizeof(*task));
+
+    if (task == NULL) {
+        return NULL;
+    }
+
+    task->title = string_duplicate(title);
+    if (task->title == NULL) {
+        free(task);
+        return NULL;
+    }
+
+    task ->id = id;
+    task->priority = priority;
+    task->status = TASK_TODO;
+    task->next = NULL;
+
+    return task;
 }
 
 void task_add(Task **head, int id, const char *title, TaskPriority priority) {

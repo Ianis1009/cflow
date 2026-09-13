f19d6906d5382815da1f1d43cf834ef8c2bff023
Iosif Ianis-Cosmin <iosifianis1@gmail.com>
2026-09-13 22:34:16 +0300

    update task.h

--- include/task.h
+++ include/task.h
@@ -27,4 +27,31 @@ typedef struct Task {
 } Task;
 
 
+Task *task_create(int id, const char *title, TaskPriority priority);
+
+
+void task_add(Task **head, int id, const char *title, TaskPriority priority);
+
+
+Task *task_find(Task *head, int id);
+
+
+int task_delete(Task **head, int id);
+int task_start(Task *head, int id);
+int task_complete(Task *head, int id);
+
+
+size_t task_count(const Task *head);
+
+
+void task_print(const Task *task);
+void task_print_all(const Task *head);
+
+
+void task_free(Task **head);
+
+const char *task_status_to_string(TaskStatus status);
+const char *task_priority_to_string(TaskPriority priority);
+
+
 #endif

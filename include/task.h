ac7fe096d54b4335907c856e6622e92b5e72ae7f
Iosif Ianis-Cosmin <iosifianis1@gmail.com>
2026-09-13 22:32:53 +0300

    update task.h

--- include/task.h
+++ include/task.h
@@ -1,1 +1,30 @@
-//TODO
+#ifndef TASK_H
+#define TASK_H
+
+
+#include <stddef.h>
+
+typedef enum {
+    TASK_TODO,
+    TASK_ACTIVE,
+    TASK_COMPLETED
+} TaskStatus;
+
+typedef enum {
+    PRIORITY_LOW,
+    PRIORITY_MEDIUM,
+    PRIORITY_HIGH
+} TaskPriority;
+
+typedef struct Task {
+
+    int id;
+    char* title;
+    TaskPriority priority;
+    TaskStatus status;
+
+    struct Task* next;
+} Task;
+
+
+#endif

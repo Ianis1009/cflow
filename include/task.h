182b9ba726cb46a2104c1d1ddce6fa04e084f3dd
Iosif Ianis-Cosmin <iosifianis1@gmail.com>
2026-09-15 22:10:00 +0300

    update task.h

--- include/task.h
+++ include/task.h
@@ -1,8 +1,8 @@
 #ifndef TASK_H
 #define TASK_H
 
-
 #include <stddef.h>
+
 
 typedef enum {
     TASK_TODO,
@@ -10,48 +10,34 @@ typedef enum {
     TASK_COMPLETED
 } TaskStatus;
 
+
 typedef enum {
     PRIORITY_LOW,
     PRIORITY_MEDIUM,
     PRIORITY_HIGH
 } TaskPriority;
 
-typedef struct Task {
 
+typedef struct Task {
     int id;
-    char* title;
+    char *title;
+
     TaskPriority priority;
     TaskStatus status;
 
-    struct Task* next;
+    struct Task *next;
 } Task;
 
 
 Task *task_create(int id, const char *title, TaskPriority priority);
 
 
-void task_add(Task **head, int id, const char *title, TaskPriority priority);
-
-
-Task *task_find(Task *head, int id);
-
-
-int task_delete(Task **head, int id);
-int task_start(Task *head, int id);
-int task_complete(Task *head, int id);
-
-
-size_t task_count(const Task *head);
-
-
-void task_print(const Task *task);
-void task_print_all(const Task *head);
-
-
-void task_free(Task **head);
-
 const char *task_status_to_string(TaskStatus status);
 const char *task_priority_to_string(TaskPriority priority);
 
+void task_print(const Task *task);
+
+void task_destroy(Task *task);
+void task_free_all(Task **head);
 
 #endif

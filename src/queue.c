e3a7f5cb39fc4cfebc4e3c2d8439fe391d13ca0d
Iosif Ianis-Cosmin <iosifianis1@gmail.com>
2026-09-15 22:17:30 +0300

    add functions in queue.c

--- src/queue.c
+++ src/queue.c
@@ -1,1 +1,22 @@
-//TODO
+#include "queue.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void queue_init (TaskQueue *queue) {
+
+    if (queue == NULL) {return;}
+
+    queue ->head = NULL;
+    queue ->tail = NULL;
+    queue ->size = 0;
+}
+
+int queue_is_empty (const TaskQueue *queue) {
+
+    if (queue == NULL) {
+        return 1;
+    }
+    return queue ->head == NULL;
+}
+
